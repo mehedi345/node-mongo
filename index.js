1dc2df4cde5bdebbc99a5924b7bf67f49d0c283a
mehedi345 <mehedihasan5101220@gmail.com>
2020-09-26 18:50:33 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+
+app.get('/', (req, res) => { res.send ('I know how to open node yaay')});
+
+app.listen(4200, ()=> console.log('Listening to port 3000'));
